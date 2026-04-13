6373dfb257e5dd264c27d9ae013d2af09b036839
Tiago Dias <tiagodiasgdl474@gmail.com>
2026-04-13 01:35:25 -0300

    Iniciado modal para adicionar novas tarefas

--- principal/script.js
+++ principal/script.js
@@ -1,3 +1,16 @@
+const modal = document.querySelector('div#div_nova_tarefa')
+const display_nova = document.querySelector('div#overlay')
+
 function adicionar () {
-    
+    display_nova.style.display = 'flex'
 }
+
+function fechar_adicionar () {
+    display_nova.style.display = 'none'
+}
+
+display_nova.addEventListener('click', function (event){
+    if (!modal.contains(event.target)) {
+        fechar_adicionar()
+    }
+})
