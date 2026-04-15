8c0ec71020789f4e4dae0212ec7cfec0b98a401c
Tiago Dias <tiagodiasgdl474@gmail.com>
2026-04-15 16:08:32 -0300

    Adição de tarefas quase pronta

--- principal/script.js
+++ principal/script.js
@@ -1,3 +1,5 @@
+
+
 const modal = document.querySelector('div#div_nova_tarefa')
 const display_nova = document.querySelector('div#overlay')
 
@@ -14,3 +16,14 @@ display_nova.addEventListener('click', function (event){
         fechar_adicionar()
     }
 })
+
+function adicao_tarefa() {
+    const text_tarefa = document.querySelector('input#text_nova_tarefa').value
+    let ul_tarefas = document.querySelector('ul#ul_tarefas')
+    var item_li = document.createElement('li')
+    item_li.textContent = text_tarefa
+    ul_tarefas.appendChild(item_li)
+
+    display_nova.style.display = 'none'
+    input.value=''
+}
