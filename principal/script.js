8f8a49d1c98321a7202a541b3e9c005411558466
Tiago Dias <tiagodiasgdl474@gmail.com>
2026-04-15 21:51:04 -0300

    Feito parte das datas e autenticação se existe algo escrito

--- principal/script.js
+++ principal/script.js
@@ -2,9 +2,12 @@
 
 const modal = document.querySelector('div#div_nova_tarefa')
 const display_nova = document.querySelector('div#overlay')
+const input = document.querySelector('input#text_nova_tarefa')
+const input_data = document.querySelector('input#text_data')
 
 function adicionar () {
     display_nova.style.display = 'flex'
+    input.focus()
 }
 
 function fechar_adicionar () {
@@ -18,12 +21,20 @@ display_nova.addEventListener('click', function (event){
 })
 
 function adicao_tarefa() {
-    const text_tarefa = document.querySelector('input#text_nova_tarefa').value
+    const text_tarefa = input.value
+    const data = input_data.value
+    const data_br = new Date(data).toLocaleDateString('pt-br')
     let ul_tarefas = document.querySelector('ul#ul_tarefas')
-    var item_li = document.createElement('li')
-    item_li.textContent = text_tarefa
-    ul_tarefas.appendChild(item_li)
+    
+    if (text_tarefa.length === 0) {
+        window.alert('Tarefa Vazia!')
+    } else{
+        var item_li = document.createElement('li')
+        item_li.textContent = `${text_tarefa} - ${data_br}`
+        ul_tarefas.appendChild(item_li)
 
     display_nova.style.display = 'none'
+    }
     input.value=''
+    input_data.value=''
 }
