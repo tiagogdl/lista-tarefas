6e2bc6298f3349224209e83fe8f522a5296c47d4
Tiago Dias <tiagodiasgdl474@gmail.com>
2026-04-17 15:55:28 -0300

    Validação de data feita

--- principal/script.js
+++ principal/script.js
@@ -4,13 +4,15 @@ const modal = document.querySelector('div#div_nova_tarefa')
 const display_nova = document.querySelector('div#overlay')
 const input = document.querySelector('input#text_nova_tarefa')
 const input_data = document.querySelector('input#text_data')
+let guarda_tarefas = []
 
-function adicionar () {
+
+function adicionar () { // Faz aparece modal para adicionar tarefa
     display_nova.style.display = 'flex'
     input.focus()
 }
 
-function fechar_adicionar () {
+function fechar_adicionar () { // Some com o modal quando feito a condição abaixo
     display_nova.style.display = 'none'
 }
 
@@ -20,21 +22,41 @@ display_nova.addEventListener('click', function (event){
     }
 })
 
-function adicao_tarefa() {
+function adicao_tarefa() { // Executa processo de adição de tarefas e datas e salva no vetor
     const text_tarefa = input.value
     const data = input_data.value
-    const data_br = new Date(data).toLocaleDateString('pt-br')
     let ul_tarefas = document.querySelector('ul#ul_tarefas')
-    
+    let data_br = new Date(data)
+
+    if (data) { //Validação se data Inválida
+        if (data_br >= new Date ()) {
+           data_br = data_br.toLocaleDateString('pt-br')
+        } else {
+            window.alert('Data Inválida')
+            input_data.value=''
+            input_data.focus()
+            return
+        }
+       var escrita_tarefa = `${text_tarefa} - ${data_br}`
+    } else {
+        escrita_tarefa = text_tarefa
+    }
+
     if (text_tarefa.length === 0) {
         window.alert('Tarefa Vazia!')
     } else{
         var item_li = document.createElement('li')
-        item_li.textContent = `${text_tarefa} - ${data_br}`
+        item_li.textContent = `${escrita_tarefa}`
         ul_tarefas.appendChild(item_li)
-
-    display_nova.style.display = 'none'
+        display_nova.style.display = 'none'
+        
+        guarda_tarefas.push({
+            tarefa: text_tarefa,
+            data: data_br
+        })
     }
+
     input.value=''
     input_data.value=''
 }
+
