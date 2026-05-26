6ab920ab6890e6e2b79ffa31536a5fb68434faee
Tiago Dias <tiagodiasgdl474@gmail.com>
2026-05-26 16:19:31 -0300

    Corrigido cores

--- TodoListVue/nuxt-app/app/components/ModalForm.vue
+++ TodoListVue/nuxt-app/app/components/ModalForm.vue
@@ -1,7 +1,7 @@
 <template>
         <div v-if="props.open" @click="emits('close')"
          class="fixed inset-0 z-50 flex justify-center items-center bg-black/50">
-                <div @click.stop="" class="w-[300px] h-[250px] rounded-2xl p-4 bg-sky-200 flex flex-col justify-between">
+                <div @click.stop="" class="w-[300px] h-[250px] rounded-2xl p-4 bg-sky-200 text-black flex flex-col justify-between">
                     <div class="grid grid-cols-[60px_1fr]">
                         <div class="flex flex-col gap-20">
                             <label for="tarefa">Tarefa: </label>
--- TodoListVue/nuxt-app/app/components/TodoMain.vue
+++ TodoListVue/nuxt-app/app/components/TodoMain.vue
@@ -1,5 +1,5 @@
 <template>
-    <UContainer class="mt-3 max-w-2xl shadow-sm">
+    <UContainer class="mt-3 max-w-2xl shadow-sm bg-white">
         <div class="overflow-hidden mb-10 w-full h-full">
             <ButtonForm @abrir="ModalAberto = true" />
             <TarefasTodo @editar="abrirEdicao" @excluir="excluirTarefa"/>
