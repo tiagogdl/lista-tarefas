16ef715fad36ec1c5017a5f96a25ac7b592cc44d
Tiago Dias <tiagodiasgdl474@gmail.com>
2026-05-26 16:24:55 -0300

    Forçar a manter tema claro

--- TodoListVue/nuxt-app/nuxt.config.ts
+++ TodoListVue/nuxt-app/nuxt.config.ts
@@ -3,5 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@pinia/nuxt'],
-  css: ['assets/css/main.css']
+  css: ['assets/css/main.css'],
+  app: {
+    head: {
+      meta: [
+        { name: 'color-scheme', content: 'light' }
+      ]
+    }
+  }
 })
