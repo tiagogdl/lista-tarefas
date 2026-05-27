c6a0bf869008b53f13d51c4ee9952808ac70fb86
Tiago Dias <tiagodiasgdl474@gmail.com>
2026-05-27 00:28:19 -0300

    Projeto Finalizado

--- TodoListVue/nuxt-app/app/components/TarefasTodo.vue
+++ TodoListVue/nuxt-app/app/components/TarefasTodo.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="flex flex-col items-center justify-center py-20 text-textSlate400 animate-pulse" v-if="UserStore.tarefas.length === 0">
+    <div class="flex flex-col items-center justify-center py-20 text-textSlate400 animate-pulse" 
+    v-if="UserStore.tarefas.length === 0">
             <UIcon name="i-heroicons-clipboard-document-list" size="70" class="text-primary mb-4"/>
 
             <p class="text-xl font-semibold">Nenhuma tarefa criada ainda</p>
@@ -7,7 +8,7 @@
             <p>Clique em "Nova tarefa" para começar</p>
     </div>
     <div v-else v-for="tarefa in UserStore.tarefas"
-    :key="tarefa.id" @click="tarefa.completed = !tarefa.completed" :class="['flex justify-between mb-8  rounded-2xl p-4 shadow-md border text-textBW border-textSlate300 transition-all duration-300 hover:scale-[1.01]', tarefa.completed ? 'bg-emerald-400 opacity-70' : 'bg-branco']">
+    :key="tarefa.id" @click="tarefa.completed = !tarefa.completed" :class="['flex justify-between mb-8  rounded-2xl p-4 shadow-md border text-textBW border-textSlate300 transition-all duration-300 hover:scale-[1.01]', tarefa.completed ? 'bg-[#22c55e] opacity-80' : 'bg-branco']">
         <div :class="{'line-through text-fundoGradi-t':tarefa.completed}">
             <p>{{ tarefa.text }}</p>
             <p v-if="tarefa.data">Data:  {{ DataPt(tarefa.data)}}</p>
